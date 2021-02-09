9507b54dff56cae1a2411956730f7828f0874911
jayanthi <jayanthi.ragu93@gmail.com>
2021-02-09 23:16:41 +0530

    'first_commit'

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 //Enter your code here..
+var btn = document.querySelector("#btnGet");
+var msg = document.querySelector("#message");
+btn.onclick = function()
+{
+    const promise = new Promise((resolve,reject) =>  {
+        const request = new XMLHttpRequest;
+        request.open('GET',"https://jsonplaceholder.typicode.com/users");
+        request.onload = ()=>{
+            if(request.status === 200)
+            {
+                resolve(request.response);
+            }
+            else
+            {
+                reject(Error(request.response));
+            }
+        }
+        request.onerror = () => {
+            reject(Error('Error fetching data'));
+        }
+        request.send();
+    });
+
+    promise.then((data) => {
+        const result = JSON.parse(data);
+        console.log(data);
+        var player='<h2>Lists of Users</h2>';
+        result.forEach(function(user) {
+            player+=
+        `<div class="player">
+            <div class="strength">Name : ${user.name}</div>
+            <div>Email   : ${user.email}</div>
+            <div>Phone   : ${user.phone}</div>
+            <div>Website : ${user.website}</div>
+            <div>Company : ${user.company.name}</div>
+            <div>City    : ${user.address.city}</div>
+            <div>Zipcode : ${user.address.zipcode}</div>
+            </div>`
+        });
+        msg.innerHTML = player;
+    },(err) => {
+        console.log('Promise rejected.');
+        console.log(error.message);
+    });
+}
